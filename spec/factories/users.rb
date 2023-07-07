2f861aa9275e4ef58e89ef8b80368c44f93424e0
upamaru0104 <upakame0104@gmail.com>
2023-07-07 20:32:07 +0900

    修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {test01}
+    password              {'test01'}
     password_confirmation {password}
     first_name            {'太郎'}
     last_name             {'佐藤'}
