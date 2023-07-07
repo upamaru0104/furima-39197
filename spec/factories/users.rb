c6845c80b7a2c4cbafd4eb12029ee2fba8bdfabb
upamaru0104 <upakame0104@gmail.com>
2023-07-07 20:15:47 +0900

    パスワード修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6, mix_case: true, special_characters: false)}
+    password              {test01}
     password_confirmation {password}
     first_name            {'太郎'}
     last_name             {'佐藤'}
