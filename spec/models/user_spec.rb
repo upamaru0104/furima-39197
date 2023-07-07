1c399d7404b4502e926b68c6b79f87ec4cfa6d94
upamaru0104 <upakame0104@gmail.com>
2023-07-07 20:21:35 +0900

    テストの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -111,6 +111,18 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Last name is invalid. Input full-width characters.')
       end
+      it '英字のみのパスワードでは登録できない' do
+        @user.password = 'abcdef'
+        @user.password_confirmation = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password は半角英字と数字を含めてください')
+      end
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'ＡＢＣＤＥＦ'
+        @user.password_confirmation = 'ＡＢＣＤＥＦ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password は半角英字と数字を含めてください')
+      end
     end
   end
 end
