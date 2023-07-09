d948bec83ba8283ac8cc56b7d2f4ba40b003149c
upamaru0104 <upakame0104@gmail.com>
2023-07-09 16:56:31 +0900

    画像投稿に関する記述
    アソシエーションとコントローラーに画像がいっそに保存されるようにストロングパラメーターを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
