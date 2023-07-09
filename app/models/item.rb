96ae5c7240f41e162c92d7748362a9446360cd13
upamaru0104 <upakame0104@gmail.com>
2023-07-09 16:13:53 +0900

    バリデーションとアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  validates :name,               presence: true
+  validates :explanation,        presence: true
+  validates :category_id,        presence: true
+  validates :condition_id,       presence: true
+  validates :delivery_charge_id, presence: true
+  validates :prefecture_id,      presence: true
+  validates :delivery_day_id,    presence: true
+  validates :price,              presence: true
+
+  belongs_to :user
+  # has_one    :purchase_record
 end
