ee7daab31912a66e8e3618c06b0f859c9e329734
upamaru0104 <upakame0104@gmail.com>
2023-07-11 17:00:12 +0900

    priceカラムについてのバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
     validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A\d+\z/, message: "は半角数値のみ入力してください" }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 ,message:"は300~9999999の範囲で入力してください"}, format: { with: /\A\d+\z/, message: "は半角数値のみ入力してください" }
     validates :image
   end
 
