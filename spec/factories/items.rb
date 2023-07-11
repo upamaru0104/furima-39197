dc92e5608252d302cee1874cbcac97692c28abc4
upamaru0104 <upakame0104@gmail.com>
2023-07-11 17:08:43 +0900

    priceカラムのバリデーションの修正
    integer型のカラムに対して、正規表現は無効となる
    priceに対するバリデーションを2つに分けて設定する

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :item do
-    
+    name{Faker::Name.initials(number: 2)}
+    explanation{"説明です"}
+    category_id{"2"}
+    condition_id{"2"}
+    delivery_charge_id{"2"}
+    delivery_day_id{"2"}
+    price{Faker::Number}
   end
 end
