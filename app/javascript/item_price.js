19c96da4a94054f0332de3f54e7fc945f677d469
upamaru0104 <upakame0104@gmail.com>
2023-07-14 16:03:27 +0900

    javascriptの価格計算実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,16 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+window.addEventListener('load', function(){
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+
+  const addTaxDom = document.getElementById("add-tax-price");
+  const addTax = inputValue * 0.1;
+  addTaxDom.innerHTML = addTax;
+
+  const profitDom = document.getElementById("profit");
+  const profit = inputValue - addTax;
+  profitDom.innerHTML = profit;
+
+  })
+})
+
