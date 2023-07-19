c35f558caa460599c0ec65d1c0f2e7b216d09c80
upamaru0104 <upakame0104@gmail.com>
2023-07-19 14:36:37 +0900

    重複するコードをまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   # ログインしていないユーザーをログインページの画面に促す
   before_action :authenticate_user!, only: [:new, :edit]
   # , :destroy]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -21,19 +22,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     # @purchase_record = @item.purchase_record
   end
 
   def edit
-    @item = Item.find(params[:id])
     if @item.user != current_user
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to action: :show, id: @item.id
     else
@@ -49,5 +47,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
