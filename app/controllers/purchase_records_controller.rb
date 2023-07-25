b97dc054d985167ab5a6af8ecad4a76daef2ad64
upamaru0104 <upakame0104@gmail.com>
2023-07-25 16:07:25 +0900

    @itemを定義

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,2 +1,16 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
 end
