91a690c672f8c9e786cbce5c34b644f30ff2ee4e
upamaru0104 <upakame0104@gmail.com>
2023-07-27 14:48:00 +0900

    購入ボタンをクリックしたら、イベントが発火
    ”button”というidを指定して、購入ボタンの要素を取得

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,9 @@
 const pay = () => {
-  console.log("カード情報トークン化のためのJavaScript");
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("フォーム送信時にイベント発火")
+  });
 };
 
 window.addEventListener("load", pay);
