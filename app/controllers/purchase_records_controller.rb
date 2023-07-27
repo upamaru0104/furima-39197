0d7b3bd6f4866009f22a94eeb459dd09a7cb45dd
upamaru0104 <upakame0104@gmail.com>
2023-07-27 14:38:59 +0900

    purchase_recordコントローラーの記述

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -9,8 +9,25 @@ class PurchaseRecordsController < ApplicationController
   end
 
   def create
+    @purchase_info = PurchaseInfo.new(purchase_params)
+    @shipping_info = ShippingInfo.new(address_params)
+  
+    if @purchase_info.valid? && @shipping_info.valid?
+      @purchase_info.save
+      @shipping_info.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def purchase_params
+    params.require(:purchase_info).permit(:item_id).merge(user_id: current_user.id)
   end
 
-  private
+  def address_params
+    params.require(:shipping_info).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number)  end
 
 end
