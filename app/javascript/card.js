6985e44196c5e67f39be43d192e8ff220b1ff0d5
upamaru0104 <upakame0104@gmail.com>
2023-07-27 15:08:02 +0900

    クレジットカード入力フォームの作成
    elements.create()で、入力フォームを作成
    mountメソッドで、引数で要素のid属性を指定し、指定した要素とelementインスタンスが情報を持つフォームとを置き換える

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,17 @@
 const pay = () => {
+  const payjp = Payjp('pk_test_***********************')// PAY.JPテスト公開鍵
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
   });
 };
 
