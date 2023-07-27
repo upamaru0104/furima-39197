daac5d9ce10fd5aefe0cea751e00c4ff7dcb7836
upamaru0104 <upakame0104@gmail.com>
2023-07-27 15:28:30 +0900

    決済処理を記述
    秘密鍵は後で記述

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -13,6 +13,12 @@ class PurchaseRecordsController < ApplicationController
     @shipping_info = ShippingInfo.new(address_params)
   
     if @purchase_info.valid? && @shipping_info.valid?
+      Payjp.api_key = "sk_test_***********"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: order_params[:price],  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
       @purchase_info.save
       @shipping_info.save
       redirect_to root_path
