29573554b08defed7dd38545284bd9c5865bdd92
upamaru0104 <upakame0104@gmail.com>
2023-07-29 15:24:34 +0900

    Update purchase_records_controller.rb
    @purchase_info = PurchaseAddress.newは空の箱を作り、@purchase_info = PurchaseAddress.new(purchase_params)で(purchase_params)のストロングパラメーターの中身の情報を持ってくる
    PurchaseAddressはフォームオブジェクト

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -3,42 +3,38 @@ class PurchaseRecordsController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @purchase_info = PurchaseAddress.new
   end
 
   def new
   end
 
   def create
-    @purchase_info = PurchaseInfo.new(purchase_params)
-    @shipping_info = ShippingInfo.new(address_params)
-  
-    if @purchase_info.valid? && @shipping_info.valid?
+    @item = Item.find(params[:item_id])
+    @purchase_info = PurchaseAddress.new(purchase_params)
+    if @purchase_info.valid?
       pay_item
       @purchase_info.save
-      @shipping_info.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
-  
+      
   private
-
+    
   def purchase_params
-    params.require(:purchase_info).permit(:item_id).merge(user_id: current_user.id, token: params[:token])
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(item_id:@item.id, user_id: current_user.id, token: params[:token])
   end
-
-  def address_params
-    params.require(:shipping_info).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number)
-  end
-
+    
   def pay_item
     Payjp.api_key = "sk_test_***********"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+    amount: order_params[:price],  # 商品の値段
+    card: order_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
     )
   end
-
+    
 end
+
