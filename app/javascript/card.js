a7d563fb8d26a3869fcae725cd788b14269b31c5
upamaru0104 <upakame0104@gmail.com>
2023-07-29 15:43:06 +0900

    購入画面の修正
    モデル名の名前が違うときはurlが必要
    model: @purchase_info, url: item_purchase_records_path,
    カード情報入力に関してはjsがする

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -5,9 +5,9 @@ const pay = () => {
   const expiryElement = elements.create('cardExpiry');
   const cvcElement = elements.create('cardCvc');
 
-  numberElement.mount('#number-form');
-  expiryElement.mount('#expiry-form');
-  cvcElement.mount('#cvc-form');
+  numberElement.mount('#card-number');
+  expiryElement.mount('#card-expiry');
+  cvcElement.mount('#card-cvc');
 
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
