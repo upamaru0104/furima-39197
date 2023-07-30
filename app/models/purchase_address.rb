c9fa235830e763b8e348be28e44ca6f795a65d02
upamaru0104 <upakame0104@gmail.com>
2023-07-30 19:39:30 +0900

    不要なバリデーション
    binding.pryまでの行ができているかを確認し次の行を手動で入力することで
    その行の記述ができているかを確認する

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -7,13 +7,11 @@ class PurchaseAddress
   with_options presence: true do
     validates :item_id
     validates :user_id
-    validates :shipping_address_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :city, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :house_number
     validates :telephone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid."}
-    validates :purchase_record_id
   end
 
   def save
