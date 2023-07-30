f16b27cba12ba72a53b87da654227692ed0407ea
upamaru0104 <upakame0104@gmail.com>
2023-07-30 19:40:04 +0900

    不要な記述

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -30,7 +30,6 @@ class PurchaseRecordsController < ApplicationController
   def pay_item
     Payjp.api_key = "sk_test_****"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
     Payjp::Charge.create(
-    amount: order_params[:price],  # 商品の値段
     card: order_params[:token],    # カードトークン
     currency: 'jpy'                 # 通貨の種類（日本円）
     )
