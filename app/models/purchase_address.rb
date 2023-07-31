cdb9b2844102d7f80825c8931273c477d75800ec
upamaru0104 <upakame0104@gmail.com>
2023-07-31 14:35:20 +0900

    バリデーションのエラーメッセージの変更

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -11,7 +11,7 @@ class PurchaseAddress
     validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :city, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :house_number
-    validates :telephone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid."}
+    validates :telephone_number, format: {with: /\A[0-9]{10,11}\z/, message: "は10桁以上11桁以内の半角数値で入力してください"}
   end
 
   def save
