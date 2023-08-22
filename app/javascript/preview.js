c97a02e5bdfca1f3838d75538f5aae40255a9bb4
upamaru0104 <upakame0104@gmail.com>
2023-08-22 10:34:22 +0900

    画像の差し替えができるよう編集、選択しなかった場合に画像が削除されるように

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -45,13 +45,38 @@ document.addEventListener('DOMContentLoaded', function(){
     fileFieldsArea.appendChild(newFileField);
   };
 
+  // 指定したdata-indexを持つプレビューとfile_fieldを削除する
+  const deleteImage = (dataIndex) => {
+    const deletePreviewImage = document.querySelector(`.preview[data-index="${dataIndex}"]`);
+    deletePreviewImage.remove();
+    const deleteFileField = document.querySelector(`input[type="file"][data-index="${dataIndex}"]`);
+    deleteFileField.remove();
+  };
+
   // input要素で値の変化が起きた際に呼び出される関数の中身
   const changedFileField = (e) => {
     // data-index（何番目を操作しているか）を取得
     const dataIndex = e.target.getAttribute('data-index');
-    
+
     const file = e.target.files[0];
+
+    // fileが空 = 何も選択しなかったのでプレビュー等を削除して終了する
+    if (!file) {
+      deleteImage(dataIndex);
+      return null;
+    }
+    
     const blob = window.URL.createObjectURL(file);
+
+    // data-indexを使用して、既にプレビューが表示されているかを確認する
+    const alreadyPreview = document.querySelector(`.preview[data-index="${dataIndex}"]`);
+
+    if (alreadyPreview) {
+      // クリックしたfile_fieldのdata-indexと、同じ番号のプレビュー画像が既に表示されている場合は、画像の差し替えのみを行う
+      const alreadyPreviewImage = alreadyPreview.querySelector("img");
+      alreadyPreviewImage.setAttribute("src", blob);
+      return null;
+    };
 
     buildPreviewImage(dataIndex, blob);
     buildNewFileField();
