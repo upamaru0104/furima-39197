8d83f28607ed35385eebdb9ffd8f583b50b8e4b8
upamaru0104 <upakame0104@gmail.com>
2023-08-29 11:26:01 +0900

    新規投稿する画像ファイルにclass名を付け、画像表示サイズを調整

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const previewImage = document.createElement('img');
     previewImage.setAttribute('class', 'preview-image');
     previewImage.setAttribute('src', blob);
+    previewImage.classList.add('preview-size');
 
     // 削除ボタンを生成
     const deleteButton = document.createElement("div");
